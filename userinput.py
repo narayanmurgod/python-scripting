9c835843d683fcc7fce596795b57d38b5f227bac
nani <murgod@google.com>
2024-03-23 08:10:50 +0000

    updated code

--- userinput.py
+++ userinput.py
@@ -2,6 +2,6 @@ name = input('Enter your name:')
 age = int(input("Enter your age:"))
 
 if age > 18:
-    print('You can vote')
+    print(f"hey {name}..! You can vote")
 else:
-    print(f"You aren't eligible to vote")
+    print(f"hey {name}..! You aren't eligible to vote")
