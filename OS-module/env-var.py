7dbae90d4fe3de78e04853ce7af3ea95d02650c6
nani <murgod@google.com>
2024-08-25 09:21:16 +0000

    environment var

--- OS-module/env-var.py
+++ OS-module/env-var.py
@@ -1,9 +1,22 @@
 import os
 
-# Get the value of the HOME environment variable
-home_dir = os.environ.get("HOME")
-print(f"HOME directory: {home_dir}")
+# Display current environment variables
+print("Current environment variables:")
+for key, value in os.environ.items():
+    print(f"{key}: {value}")
 
-# Get the value of the PATH environment variable
-path_var = os.environ.get("PATH")
-print(f"PATH variable: {path_var}")
+# Define the environment variable name and value
+env_var_name = "MY_ENV_VAR"
+env_var_value = "MyValue123"
+
+# Set the environment variable using os.putenv()
+os.putenv(env_var_name, env_var_value)
+
+# Retrieve the environment variable using os.getenv()
+retrieved_value = os.getenv(env_var_name)
+print(f"\nEnvironment variable '{env_var_name}' set to '{retrieved_value}'")
+
+# Display updated environment variables
+print("\nUpdated environment variables:")
+for key, value in os.environ.items():
+    print(f"{key}: {value}")
