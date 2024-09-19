a7aaa6bfe3560092ff32551a4e90832aa50ad16b
narayanmurgod <narayanmurgod3388@gmail.com>
2024-09-19 15:43:54 +0000

    run function

---
+++ subprocess-module/run_function.py
@@ -0,0 +1,14 @@
+import subprocess
+
+result = subprocess.run(["python", "intro.txt"], capture_output=True, text=True)
+
+print(result.stdout)
+
+#if file isn't present
+
+
+result = subprocess.run(["python", "file_donot_exist.py"], capture_output=True, text=True, check=False)
+
+print(result.stdout)
+
+print(result.stderr)
