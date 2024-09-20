84439a09d4a2f26189ad111ca118962474aba6bc
narayanmurgod <narayanmurgod3388@gmail.com>
2024-09-20 17:44:15 +0000

    added pipe fuctioned program

---
+++ subprocess-module/pipe.py
@@ -0,0 +1,13 @@
+import subprocess
+
+ls_process = subprocess.Popen(["ls"], stdout=subprocess.PIPE, text=True)
+
+grep_process = subprocess.Popen(["grep", "open"], stdin=ls_process.stdout, stdout=subprocess.PIPE, text=True)
+
+output, error = grep_process.communicate()
+
+print(output)
+
+print(error)
+
+#Python subprocess module provides a way to create and interact with child processes, which can be used to run other programs or commands. One of the features of the subprocess module is the ability to create pipes, which allow communication between the parent and child processes.
